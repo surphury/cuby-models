7cbbef49e1d4d5ff6fed9fb7db32d494e9044d38
Jose Manuel <jose@261004@gmail.com>
2021-11-08 21:13:59 -0400

    refactor: now using a clouser

--- slider.js
+++ slider.js
@@ -1,24 +1,27 @@
+/* All HTMLElements have a "$" at the beginning */
 (() => {
-'use strict';
-const $carousel = document.getElementById('carousel');
-const $carouselImages = $carousel.querySelector('.carousel__images');
-const imagesLength = $carouselImages.childElementCount;
-let counter = 0;
+    'use strict';
+    const $carousel = document.getElementById('carousel'),
+        $carouselImages = $carousel.querySelector('.carousel__images'),
+        $rightButton = $carousel.querySelector('.carousel__btn--right'),
+        $leftButton = $carousel.querySelector('.carousel__btn--left');
 
-const slide = (next = true) => {
-    if (next && counter < imagesLength - 1)
-        counter++;
-    else if (!next && counter > 0)
-        counter--;
+    const imagesLength = $carouselImages.childElementCount;
 
-    $carouselImages.style.setProperty('--counter', counter);
-}
+    const slide = () => {
+        let counter = 0;
+        
+        return (next = false) => {
+            if (next && counter < imagesLength - 1)
+            counter++;
+            else if (!next && counter > 0)
+            counter--;
+    
+            $carouselImages.style.setProperty('--counter', counter);
+        }
+    }
 
-$carousel.addEventListener('click', (e) => {
-    const $E = e.target;
-    if ($E.classList.contains('carousel__btn--right'))
-        slide()
-    else if ($E.classList.contains('carousel__btn--left'))
-        slide(false);
-})
+    $carousel.slide = slide();
+    $rightButton.addEventListener('click', () => $carousel.slide(true));
+    $leftButton.addEventListener('click', () => $carousel.slide())
 })();
